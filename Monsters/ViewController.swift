905f39a82116799d0e138ffbe6bcf6ee9e511589
Alex <sanya1444@gmail.com>
2020-10-03 22:56:08 +0300

    Initial Commit

---
+++ Monsters/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Monsters
+//
+//  Created by Oleksandr Karpenko on 03.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
